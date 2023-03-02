4c7b4e9af0afb696f4b54cdff354f33c95aec25a
muratAVC <muratriyum@gmail.com>
2023-03-02 20:17:21 +0300

    amiral Battı, Yeni versiyon.tablo ekledim(Öğreniyorum)

--- src/Amirall/AmiralPen.java
+++ src/Amirall/AmiralPen.java
@@ -1,6 +1,8 @@
 package Amirall;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class AmiralPen {
     JFrame frame;
@@ -12,14 +14,35 @@ public class AmiralPen {
         JTextField textField1=new JTextField();
         JLabel label=new JLabel("Enter Rov");
         JLabel label1=new JLabel("Enter Column");
-        label.setBounds(5,20,50,20);
-        textField.setBounds(10,20,50,17);
+        label.setBounds(10,5,60,20);
+        textField.setBounds(10,23,60,17);
 
-        label1.setBounds(50,10,50,20);
-        textField1.setBounds(60,20,50,17);
+        label1.setBounds(70,5,70,20);
+        textField1.setBounds(70,23,60,17);
 
-        button.setBounds(110,20,100,19);
+        button.setBounds(140,20,100,19);
 
+        JTable table=new JTable(10,10);
+        table.setRowHeight(10);
+        table.setBounds(10,50,150,100);
+        table.setCellSelectionEnabled(true);
+
+
+
+        ListSelectionModel selectionModel=table.getSelectionModel();
+        selectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        selectionModel.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+
+                System.out.println();
+            }
+        });
+
+
+
+
+        frame.add(table);
         frame.add(button);
         frame.add(textField);
         frame.add(textField1);
